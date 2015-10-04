d62a1be3599c570390e4a39151f5ecdee2b3ea81
Rajesh Raheja <rajesh.raheja@gmail.com>
2015-10-04 00:26:05 -0700

    Fixes for multiple CrashPlan upgrade dirs

--- crashplanfix.sh
+++ crashplanfix.sh
@@ -2,7 +2,7 @@
 # CrashPlan fix for Synology package upgrades that keep breaking and stopping CrashPlan
 # Rajesh Raheja
 # October 2015
-# Usage: Run this and pass the crashplan update number (the long number) as input e.g. curl -Ls <scriptURI> | sh
+# Usage: Run this and pass the crashplan update number (the long number) as input e.g. curl -Ls <scriptURI> | sh -s <UpgradeNumber>
 
 echo The following upgrades are found:
 ls -l /var/packages/CrashPlan/target/upgrade/*.jar
@@ -20,17 +20,14 @@ fi
 unzip -o /var/packages/CrashPlan/target/upgrade/${upgradenum}.jar "*.jar" -d /var/packages/CrashPlan/target/lib/
 unzip -o /var/packages/CrashPlan/target/upgrade/${upgradenum}.jar "lang/*" -d /var/packages/CrashPlan/target/
 
-upgradedirs=`ls -ld /var/packages/CrashPlan/target/upgrade/${upgradenum}.*/ | wc -l`
-upgradedir=`ls -d /var/packages/CrashPlan/target/upgrade/${upgradenum}.*/`
-echo Folders to process: $upgradedirs
-if [ "${upgradedirs}" -gt 1 ];
-then
-	echo "More than one upgrade directory. Manually fix files using: mv <dir>/upgrade.sh <dir>/upgrade.sh.old"
-	echo ${upgradedir}
-	exit 1;
-fi
+upgradedirlist=`ls -d /var/packages/CrashPlan/target/upgrade/${upgradenum}.*/`
+echo Folders to process: `echo $upgradedirlist | wc -l`
 
-echo Processing folder ${upgradedir}
-mv ${upgradedir}/upgrade.sh ${upgradedir}/upgrade.sh.old 
+for upgradedir in $upgradedirlist ;
+do
+	echo Processing folder ${upgradedir}
+	mv ${upgradedir}/upgrade.sh ${upgradedir}/upgrade.sh.old 
+done
+	
 echo The contents of /var/lib/crashplan/.ui_info file: `cat /var/lib/crashplan/.ui_info ; echo`
 echo Restart CrashPlan service and check logs.

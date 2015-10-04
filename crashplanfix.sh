b64aad5e20d4c00f8bb0359534480ee8051cfc5a
Rajesh Raheja <rajesh.raheja@gmail.com>
2015-10-03 17:44:23 -0700

    List upgrades

--- crashplanfix.sh
+++ crashplanfix.sh
@@ -6,19 +6,29 @@
 
 echo The following upgrades are found:
 ls -l /var/packages/CrashPlan/target/upgrade/*.jar
-read -p "Enter CrashPlan Upgrade Number to process:" upgradenum
 
-if [ -z "$upgradenum" ];
+if [ -z "$1" ];
 then
 	echo "No CrashPlan Upgrade Number provided"
 	exit 1;
+else
+	upgradenum = $1
 fi
 
 unzip -o /var/packages/CrashPlan/target/upgrade/${upgradenum}.jar "*.jar" -d /var/packages/CrashPlan/target/lib/
 unzip -o /var/packages/CrashPlan/target/upgrade/${upgradenum}.jar "lang/*" -d /var/packages/CrashPlan/target/
+
+upgradedirs=`ls -ld /var/packages/CrashPlan/target/upgrade/${upgradenum}.*/ | wc -l`
+if [ $upgradedirs > 1 ];
+then
+	echo More than one upgrade directory found. In that case, simply rename upgrade.sh to upgrade.sh.old in each of those directories.
+	echo "mv <dir>/upgrade.sh <dir>/upgrade.sh.old 
+	echo $upgradedirs
+	exit 1;
+fi
+
 upgradedir=`ls -d /var/packages/CrashPlan/target/upgrade/${upgradenum}.*/`
-echo The following is the upgrade directory found. If more than one found, the script will fail. In that case, simply rename upgrade.sh to upgrade.sh.old in each of those directories.
-echo $upgradedir
+echo Processing folder $upgradedir
 mv ${upgradedir}/upgrade.sh ${upgradedir}/upgrade.sh.old 
 echo The contents of /var/lib/crashplan/.ui_info file: `cat /var/lib/crashplan/.ui_info  ; echo`
 echo Restart CrashPlan service and check logs.
